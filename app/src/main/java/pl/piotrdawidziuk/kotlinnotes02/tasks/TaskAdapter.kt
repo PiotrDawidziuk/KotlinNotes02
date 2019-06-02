aae28f4a1644726f824d991992cd0752b7897322
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-02 09:51:53 +0200

    view holder class added

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
@@ -1,12 +1,15 @@
 package pl.piotrdawidziuk.kotlinnotes02.tasks
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import pl.piotrdawidziuk.kotlinnotes02.R
 
 class TaskAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false))
+
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -16,5 +19,9 @@ class TaskAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+
+    class ViewHolder (val view: View): RecyclerView.ViewHolder(view){
+
+    }
 
 }
