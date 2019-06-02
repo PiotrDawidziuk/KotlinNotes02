c87091e8ecfeb8c096bfbac817b0f6df2d8a4303
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-02 19:43:42 +0200

    new stirngs

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
@@ -1,30 +1,49 @@
 package pl.piotrdawidziuk.kotlinnotes02.tasks
 
-import android.graphics.Paint
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_task.view.*
-import kotlinx.android.synthetic.main.view_todo.view.*
+import kotlinx.android.synthetic.main.view_add_button.view.*
 import pl.piotrdawidziuk.kotlinnotes02.R
 import pl.piotrdawidziuk.kotlinnotes02.foundations.BaseRecyclerAdapter
 import pl.piotrdawidziuk.kotlinnotes02.models.Task
 import pl.piotrdawidziuk.kotlinnotes02.views.TaskView
-import pl.piotrdawidziuk.kotlinnotes02.views.TodoView
 
 class TaskAdapter(
     taskList: MutableList<Task> = mutableListOf()
 
 ): BaseRecyclerAdapter<Task>(taskList) {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false))
 
-    class ViewHolder (view: View): BaseViewHolder<Task>(view){
+    override fun getItemViewType(position: Int): Int = if (position==0){
+        TYPE_AND_BUTTON
+    } else { TYPE_INFO
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = if(viewType == TYPE_INFO){
+        TaskViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false))
+    }else{
+        AddButtonViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.view_add_button,parent,false))
+    }
+
+    override fun getItemCount(): Int = masterList.size + 1
+
+
+    class TaskViewHolder (view: View): BaseViewHolder<Task>(view){
         override fun onBind(data: Task) {
             (view as TaskView).initView(data)
         }
+    }
+
+    class AddButtonViewHolder (view: View): BaseViewHolder<Unit>(view){
+        override fun onBind(data: Unit) {
+            view.buttonText.text =
+        }
+    }
+
+    companion object {
+        const val TYPE_AND_BUTTON =0
+        const val TYPE_INFO = 1
     }
 
 }
