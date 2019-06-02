42ede64f74ee7c5be12355921cdb35656fc82818
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-02 12:26:05 +0200

    BaseRecyclerAdapter class added and Task Adapter reformated

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskAdapter.kt
@@ -6,28 +6,20 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_task.view.*
 import pl.piotrdawidziuk.kotlinnotes02.R
+import pl.piotrdawidziuk.kotlinnotes02.foundations.BaseRecyclerAdapter
 import pl.piotrdawidziuk.kotlinnotes02.models.Task
 
 class TaskAdapter(
-    private val taskList: MutableList<Task> = mutableListOf()
+    taskList: MutableList<Task> = mutableListOf()
 
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+): BaseRecyclerAdapter<Task>(taskList) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false))
 
-    override fun getItemCount(): Int = taskList.size
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).onBind(taskList[position])
-    }
-
-
-    class ViewHolder (val view: View): RecyclerView.ViewHolder(view){
-
-        fun onBind(task: Task){
-            view.titleView.text = task.title
+    class ViewHolder (view: View): BaseViewHolder<Task>(view){
+        override fun onBind(data: Task) {
+            view.titleView.text = data.title
         }
-
     }
 
 }
