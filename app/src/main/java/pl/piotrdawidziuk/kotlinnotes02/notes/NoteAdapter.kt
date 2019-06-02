eb40ac60f539b404ad9b2d0f70095423b3bdae10
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-02 12:32:08 +0200

    NoteAdapter extends BaseRecyclerAdapter

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/notes/NoteAdapter.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/notes/NoteAdapter.kt
@@ -7,27 +7,20 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_note.view.*
 import kotlinx.android.synthetic.main.item_task.view.*
 import pl.piotrdawidziuk.kotlinnotes02.R
+import pl.piotrdawidziuk.kotlinnotes02.foundations.BaseRecyclerAdapter
 import pl.piotrdawidziuk.kotlinnotes02.models.Note
+import pl.piotrdawidziuk.kotlinnotes02.models.Task
 
 class NoteAdapter(
-    private val noteList: MutableList<Note> = mutableListOf()
+    noteList: MutableList<Note> = mutableListOf()
 
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+): BaseRecyclerAdapter<Note>(noteList){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder  =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_note,parent,false))
 
-    override fun getItemCount(): Int = noteList.size
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).onBind(noteList[position])
-    }
-
-
-    class ViewHolder (val view: View): RecyclerView.ViewHolder(view){
-
-        fun onBind(note: Note){
-            view.descriptionView.text = note.description
+    class ViewHolder (view: View): BaseViewHolder<Note>(view){
+        override fun onBind(data: Note) {
+            view.descriptionView.text = data.description
         }
-
     }
 }
