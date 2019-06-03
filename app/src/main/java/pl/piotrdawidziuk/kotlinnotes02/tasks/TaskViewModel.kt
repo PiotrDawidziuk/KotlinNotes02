9919da305c5dfe34049269a79bab3811a9738a15
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-03 16:53:31 +0200

    fake data

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskViewModel.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskViewModel.kt
@@ -1,7 +1,22 @@
 package pl.piotrdawidziuk.kotlinnotes02.tasks
 
 import androidx.lifecycle.ViewModel
+import pl.piotrdawidziuk.kotlinnotes02.models.Task
+import pl.piotrdawidziuk.kotlinnotes02.models.Todo
 
 class TaskViewModel : ViewModel() {
+
+    fun getFakeData(): List<Task> = mutableListOf(
+        Task("Testing 1", mutableListOf(
+            Todo("Test One!",true),
+            Todo("Test Two!")
+        )),
+        Task("Testing 2", mutableListOf(
+            Todo("Test Three!"),
+            Todo("Test Four!",true)
+        )) 
+    )
+
+
 
 }
