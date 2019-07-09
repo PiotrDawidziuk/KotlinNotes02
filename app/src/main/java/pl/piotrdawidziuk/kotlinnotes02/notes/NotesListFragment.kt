3ba65e3f82017b634a23c79ee4bee34c3f84c6b3
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-07-09 18:30:51 +0200

    delete oncreatae override NotesListFragment

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/notes/NotesListFragment.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/notes/NotesListFragment.kt
@@ -30,9 +30,6 @@ class NotesListFragment : Fragment() {
             }
         }
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
