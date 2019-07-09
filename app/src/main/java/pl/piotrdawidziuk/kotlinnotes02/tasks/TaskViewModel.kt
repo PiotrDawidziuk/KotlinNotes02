92d476c031882ad4018e537dffba8388dbdb59e2
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-07-09 19:13:57 +0200

    taskViewModel livedata

--- app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskViewModel.kt
+++ app/src/main/java/pl/piotrdawidziuk/kotlinnotes02/tasks/TaskViewModel.kt
@@ -1,10 +1,21 @@
 package pl.piotrdawidziuk.kotlinnotes02.tasks
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import pl.piotrdawidziuk.kotlinnotes02.models.Task
 import pl.piotrdawidziuk.kotlinnotes02.models.Todo
 
 class TaskViewModel : ViewModel() {
+
+    private val _taskListLiveData: MutableLiveData<List<Task>> = MutableLiveData()
+    val taskListLiveData: LiveData<List<Task>> = _taskListLiveData
+
+    init {
+
+        _taskListLiveData.postValue(getFakeData())
+
+    }
 
     fun getFakeData(): MutableList<Task> = mutableListOf(
         Task("Testing 1", mutableListOf(
